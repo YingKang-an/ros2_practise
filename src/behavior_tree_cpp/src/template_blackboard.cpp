e261619fb04c979e25951bc5df9d6190f539a252
YingKang-an <kanganyin911@gmail.com>
2026-05-29 19:33:12 +0800

    update

--- src/behavior_tree_cpp/src/template_blackboard.cpp
+++ src/behavior_tree_cpp/src/template_blackboard.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <filesystem>
+#include <thread>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <behaviortree_cpp/bt_factory.h>
+#include "behaviortree_cpp/loggers/groot2_publisher.h"
+
+using namespace std::chrono_literals;
 
 struct Position2D
 {
@@ -9,7 +13,7 @@ struct Position2D
   double y;
 };
 
-// 为了让 XML 加载器能把字符串变成 Position2D 这种自定义类型
+// 为了让 "XML加载器" 能把字符串变成 Position2D 这种自定义类型
 // 我们必须自己写一个转换函数,告诉BehaviorTree怎么把字符串→转成Position2D
 namespace BT
 {
@@ -27,75 +31,88 @@ template <> inline Position2D convertFromString(StringView str) {
 
 } // namespace BT
 
-class TemplateBlackboard : public BT::SyncActionNode
-{
+class SetTemplateBlackboard : public BT::SyncActionNode {
 public:
-  TemplateBlackboard(const std::string& name, const BT::NodeConfig& cfg)
+ SetTemplateBlackboard(const std::string& name, const BT::NodeConfig& cfg)
   : SyncActionNode(name, cfg)
   {
   }
 
   static BT::PortsList providedPorts() {
     return {
-      BT::OutputPort<Position2D>("point2f"),
+      BT::OutputPort<Position2D>("points"),
       BT::OutputPort<int>("hp")
     };
   }
 
-    BT::NodeStatus tick() override
-    {
-        Position2D pos {10.5, 20.7};
-        int hp = 99;
-        setOutput("point2f", pos);
-        setOutput("hp", hp);
-        std::cout << "写入：x=" << pos.x << " y=" << pos.y << " hp=" << hp << std::endl;
-        return BT::NodeStatus::SUCCESS;
-    }
+  BT::NodeStatus tick() override {
+    std::this_thread::sleep_for(2s);
+    Position2D pos {10.5, 30.23};
+    int hp = 99;
+    setOutput("points", pos);
+    setOutput("hp", hp);
+    std::cout << "写入：x=" << pos.x << " y=" << pos.y << " hp=" << hp << std::endl;
+    return BT::NodeStatus::SUCCESS;
+  }
 };
 
-class ReadBlackboard : public BT::SyncActionNode
-{
+class ReadPoints : public BT::SyncActionNode {
 public:
-    ReadBlackboard(const std::string& name, const BT::NodeConfig& cfg)
-        : BT::SyncActionNode(name, cfg) {}
+  ReadPoints(const std::string& name, const BT::NodeConfig& cfg)
+    : BT::SyncActionNode(name, cfg)
+  {
+  }
 
-    static BT::PortsList providedPorts()
-    {
-        return { BT::InputPort<BT::Any>("target") };
-    }
+  static BT::PortsList providedPorts() {
+    return {
+      BT::InputPort<Position2D>("points")
+    };
+  }
 
-    BT::NodeStatus tick() override
-    {
-        auto res = getInput<BT::Any>("target");
-        if (!res) return BT::NodeStatus::FAILURE;
+  BT::NodeStatus tick() override {
+    std::this_thread::sleep_for(2s);
+    auto res = getInput<Position2D>("points");
+    if (!res) return BT::NodeStatus::FAILURE;
+    auto value = res.value();
+    std::cout << "读到 Position2D: x=" << value.x << " y=" << value.y << std::endl;
+    return BT::NodeStatus::SUCCESS;
+  }
+};
 
-        // 读取 int
-        try {
-            int value = res.value().cast<int>();
-            std::cout << "读到 int: " << value << std::endl;
-            return BT::NodeStatus::SUCCESS;
-        } catch (...) {}
+class ReadHp : public BT::SyncActionNode {
+public:
+  ReadHp(const std::string& name, const BT::NodeConfig& cfg)
+    : BT::SyncActionNode(name, cfg)
+  {
+  }
 
-        // 读取 Position2D
-        try {
-            Position2D value = res.value().cast<Position2D>();
-            std::cout << "读到 Position2D: x=" << value.x << " y=" << value.y << std::endl;
-            return BT::NodeStatus::SUCCESS;
-        } catch (...) {}
+  static BT::PortsList providedPorts() {
+    return {
+      BT::InputPort<int>("hp")
+    };
+  }
 
-        return BT::NodeStatus::SUCCESS;
-    }
+  BT::NodeStatus tick() override {
+    std::this_thread::sleep_for(2s);
+    auto res = getInput<int>("hp");
+    if (!res) return BT::NodeStatus::FAILURE;
+    std::cout << "读到 int: " << res.value() << std::endl;
+    return BT::NodeStatus::SUCCESS;
+  }
 };
 
 int main() {
   BT::BehaviorTreeFactory factory;
-  factory.registerNodeType<TemplateBlackboard>("TemplateBlackboard");
-  factory.registerNodeType<ReadBlackboard>("ReadBlackboard");
+  factory.registerNodeType<SetTemplateBlackboard>("SetTemplateBlackboard");
+  factory.registerNodeType<ReadPoints>("ReadPoints");
+  factory.registerNodeType<ReadHp>("ReadHp");
 
   auto path = ament_index_cpp::get_package_share_directory(
       "behavior_tree_cpp") + "/trees/template_blackboard.xml";
   auto tree = factory.createTreeFromFile(path);
+  BT::Groot2Publisher publish(tree, 5555);
   tree.tickWhileRunning();
-
+  std::this_thread::sleep_for(2s);
+  
   return 0;
 }
